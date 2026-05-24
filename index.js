3c2bc84d7051cb553eef36c302142cd48b0f73e0
dhediyusuf77-star <dhediyusuf77@gmail.com>
2026-05-24 15:48:33 +0700

    Add mindmap structure for research framework

---
+++ index.js
@@ -0,0 +1,50 @@
+mindmap
+  root((<b>KERANGKA PIKIR PENELITIAN</b>))
+    Objek Penelitian
+      ::icon(fa fa-instagram)
+      <b>AKUN INSTAGRAM @WARGABANUA</b>
+      Sebagai Wadah (Media) Jurnalisme Warga di Kalimantan Selatan
+    Konteks Media
+      ::icon(fa fa-users)
+      <b>MEDIA JURNALISME WARGA</b>
+      Masyarakat sebagai Prosumer (Produsen & Konsumen Informasi)
+      Penyebaran Informasi Real-Time & Nilai Berita Proximity
+    Rumusan Masalah
+      ::icon(fa fa-question-circle)
+      1. Bagaimana karakteristik Pengelolaan Informasi (Gatekeeping) oleh Admin?
+      2. Bagaimana karakteristik Respon Audiens berdasarkan pemenuhan kebutuhan?
+    Pisau Analisis 1
+      ::icon(fa fa-filter)
+      <b>TEORI GATEKEEPING (Shoemaker & Vos)</b>
+      Untuk membedah mekanisme penyaringan informasi dari warga
+      <b>Unit Analisis:</b>
+        Caption (Teks)
+        Konten Visual (Foto/Video)
+        Proses Verifikasi & Editing
+      <b>5 Level Analisis:</b>
+        Individu, Rutinitas, Organisasi, Institusi, & Sistem Sosial
+    Pisau Analisis 2
+      ::icon(fa fa-comment-dots)
+      <b>TEORI USES & GRATIFICATIONS</b>
+      Untuk mengidentifikasi motif dan kepuasan audiens
+      <b>Unit Analisis:</b>
+        Kolom Komentar (Comments)
+        Likes & Engagement
+        Interaksi Antar-Warga
+      <b>Kategori Respon:</b>
+        Surveillance (Pengawasan)
+        Identitas Personal
+        Integrasi & Interaksi Sosial
+        Diversi (Hiburan)
+    Metode Penelitian
+      ::icon(fa fa-cogs)
+      <b>ANALISIS ISI KUALITATIF</b>
+      (Model Miles & Huberman)
+      Kondensasi Data
+      Penyajian Data (Display)
+      Penarikan Kesimpulan & Verifikasi
+    Hasil Penelitian
+      ::icon(fa fa-check-double)
+      <b>HASIL AKHIR</b>
+      1. Gambaran Objektif Mekanisme Gatekeeping Digital di @Wargabanua
+      2. Tipologi Respon & Kepuasan Informasi Masyarakat Kalimantan Selatan
